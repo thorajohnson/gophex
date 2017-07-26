1c2f18984989f58a77c0a2591b0637a9111b5332
kolljohnson <kjohnson@pillartechnology.com>
2017-07-26 16:46:36 -0500

    Add Getting File List from Server Test

--- test/gophex_test.exs
+++ test/gophex_test.exs
@@ -17,4 +17,15 @@ defmodule GophexTest do
     assert {:ok, socket} = :gen_tcp.connect({127, 0, 0, 1}, 4040, [:binary, packet: 0, active: :once])
     :ok = :gen_tcp.close(socket)
   end
+
+  test "Sending empty string brings back file list on server" do
+    Gophex.spawn_main()
+    send :main, {:menu, self()}
+    receive do
+      files_list ->
+	assert is_binary(files_list)
+    after
+      1_000 -> assert false
+    end
+  end
 end

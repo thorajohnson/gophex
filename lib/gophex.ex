0a7980cbd908abbfb17cd02a31c40803a52ab341
kolljohnson <kjohnson@pillartechnology.com>
2017-07-31 16:22:24 -0500

    Another Small refactor

--- lib/gophex.ex
+++ lib/gophex.ex
@@ -11,30 +11,15 @@ defmodule Gophex.Worker do
   #    shutdown: 500
   #  }
   #end
-
-  #def init(_arg) do
-  #  spawn_main()
-  #end
   
   def accept(port) do
     {:ok, socket} = :gen_tcp.listen(port,
       [:binary, packet: 0, active: false])
     Logger.info "Accepting connections on port #{port}"
-    #spawn_main(:main)
     loop_accept(socket)
   end
 
   defp loop_accept(socket) do
     {:ok, client} = :gen_tcp.accept(socket)
   end
-
-  #def spawn_main() do
-  #  main_process = spawn_link(fn () -> main({:init, "files"}) end)
-  #  Process.register(main_process, :main)
-  #end
-
-  #def spawn_main(pid_identifier) do
-  #  main_pid = spawn_link(fn () -> main({:init, "files"}) end)
-  #  Process.register(main_pid, pid_identifier) 
-  #end
 end

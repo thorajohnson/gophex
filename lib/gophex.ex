a2957a900b4ac0b9431c812a50436beb0d3e8baf
kolljohnson <kjohnson@pillartechnology.com>
2017-07-31 13:12:08 -0500

    Refactor

--- lib/gophex.ex
+++ lib/gophex.ex
@@ -1,16 +1,93 @@
-defmodule Gophex do
+defmodule Gophex.Worker do
   use Application
   require Logger
 
-  defp main({:init, "files"}) do
-    
+  #def child_spec(opts) do
+  #  %{
+  #    id: __MODULE__,
+  #    start: {__MODULE__, :accept, [opts]},
+  #    type: :worker,
+  #    restart: :temporary,
+  #    shutdown: 500
+  #  }
+  #end
+
+  #def init(_arg) do
+  #  spawn_main()
+  #end
+
+  defp main({:init, menu}) do
+    parsed_menu = parse_menu(:init, menu)
+    main(parsed_menu)
   end
 
+  defp main(state) do
+    receive do
+      {:menu, pid} ->
+	files_list = extract_file_list(state)
+	send pid, files_list
+    end
+  end
+
+  defp parse_menu(:init, menu) do
+    {:ok, file_list} = File.ls(menu)
+    parse_menu(menu, file_list)
+  end
+
+  defp parse_menu(menu, file_list) do
+    case file_list do
+      [] ->
+	[]
+      [ head ] ->
+	parsed_file = parse_file(menu, head)
+	case parsed_file do
+	  {file_name, {_, %File.Stat{type: :directory}}} ->
+	    [parsed_file] ++ parse_menu(:init, menu <> "/" <> file_name)
+	    
+	  {_, {_, %File.Stat{type: :regular}}} ->
+	    [parsed_file]
+	end
+	
+      [ head | tail ] ->
+	parsed_file = parse_file(menu, head)
+	case parsed_file do
+	  {file_name, {_, %File.Stat{type: :directory}}} ->
+	    [parsed_file] ++ parse_menu(:init, menu <> "/" <> file_name) ++
+	      parse_menu(menu, tail)
+
+	  {_, {_, %File.Stat{type: :regular}}} ->
+	    [parsed_file] ++ parse_menu(menu, tail)
+	end
+    end
+  end
+
+  def parse_file(menu, file) do
+    path = menu <> "/" <> file
+    case File.stat(path) do
+      {:ok, data} ->
+	{file, {menu, data}}
+      error ->
+	{:error, error}
+    end
+  end
+
+  def extract_file_list(directory_list) do
+    directory_list
+    |> Enum.filter(fn(file_info) ->
+      case file_info do
+	{_, {"files", _}} ->
+	  true
+	_Other ->
+	  false
+      end
+    end)
+  end
+  
   def accept(port) do
     {:ok, socket} = :gen_tcp.listen(port,
     [:binary, packet: 0, active: false])
     Logger.info "Accepting connections on port #{port}"
-    spawn_main()
+    spawn_main(:main)
     loop_accept(socket)
   end
 
@@ -18,8 +95,13 @@ defmodule Gophex do
     {:ok, client} = :gen_tcp.accept(socket)
   end
 
-  def spawn_main() do
-    main_process = spawn(fn () -> main({:init, "files"}) end)
-    Process.register(main_process, :main)
+  #def spawn_main() do
+  #  main_process = spawn_link(fn () -> main({:init, "files"}) end)
+  #  Process.register(main_process, :main)
+  #end
+
+  def spawn_main(pid_identifier) do
+    main_pid = spawn_link(fn () -> main({:init, "files"}) end)
+    Process.register(main_pid, pid_identifier) 
   end
 end
--- test/gophex_test.exs
+++ test/gophex_test.exs
@@ -1,31 +1,42 @@
-defmodule GophexTest do
-  use ExUnit.Case, async: true
-  doctest Gophex
+defmodule Gophex.GophexTest do
+  use ExUnit.Case, async: false
+  
+  #setup do
+  #  {:ok, gophex} = start_supervised(%{id: Gophex.Worker, start: {Gophex.Worker, :init, [4040]}})
+ #   %{gophex: gophex}
+ # end
 
-  test "Main process is spawned" do
-    Gophex.spawn_main()
-    assert is_pid(Process.whereis(:main))
+  test "Main process is spawned"  do
+    Gophex.Worker.spawn_main(:main_process)
+    main_pid = Process.whereis(:main_process)
+    assert is_pid(main_pid)
+    Process.exit(main_pid, :kill)
   end
 
   test "Supervisor is spawned when server is started" do
-    Gophex.Supervisor.start("", "")
+    supervisor_pid = Gophex.Supervisor.start("", "")
     assert is_pid(Process.whereis(Gophex.Supervisor))
   end
 
   test "can establish a TCP connection with Gophex" do
-    spawn fn -> Gophex.accept(4040) end
+    worker_pid = spawn fn -> Gophex.Worker.accept(4040) end
     assert {:ok, socket} = :gen_tcp.connect({127, 0, 0, 1}, 4040, [:binary, packet: 0, active: :once])
     :ok = :gen_tcp.close(socket)
+    Process.exit(worker_pid, :kill)
   end
 
   test "Sending empty string brings back file list on server" do
-    Gophex.spawn_main()
-    send :main, {:menu, self()}
+    Gophex.Worker.spawn_main(:test_main)
+    main_pid = Process.whereis(:test_main)
+    send :test_main, {:menu, self()}
     receive do
       files_list ->
-	assert is_binary(files_list)
+	assert is_list(files_list)
+	Process.exit(main_pid, :kill)
     after
-      1_000 -> assert false
+      1_000 ->
+	assert false
+	Process.exit(main_pid, :kill)
     end
   end
 end

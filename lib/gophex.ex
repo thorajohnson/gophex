6cdc0828ce346b6905dfe75e4b4ee52ce2f4fca5
kolljohnson <kjohnson@pillartechnology.com>
2017-08-09 16:42:37 -0500

    Refactor

--- lib/gophex.ex
+++ lib/gophex.ex
@@ -1,16 +1,6 @@
 defmodule Gophex.Worker do
   use Application
   require Logger
-
-  #def child_spec(opts) do
-  #  %{
-  #    id: __MODULE__,
-  #    start: {__MODULE__, :accept, [opts]},
-  #    type: :worker,
-  #    restart: :temporary,
-  #    shutdown: 500
-  #  }
-  #end
   
   def accept(port) do
     {:ok, socket} = :gen_tcp.listen(port,
@@ -21,19 +11,8 @@ defmodule Gophex.Worker do
 
   defp loop_accept(loop_socket) do
     {:ok, client} = :gen_tcp.accept(loop_socket)
-    # {:ok, pid} = Task.Supervisor.start_child(Gophex.TaskSupervisor, fn -> loop(client) end)
-    # :ok = :gen_tcp.controlling_process(client, pid)
-    #:inet.setopts(client, [:binary, packet: 0, active: :once, reuseaddr: true])
     :gen_tcp.controlling_process(client, spawn fn () -> loop(client) end)
     loop_accept(loop_socket)
-    #case :gen_tcp.accept(loop_socket) do
-    #  {:ok, socket} ->
-    #	:gen_tcp.controlling_process(socket, spawn fn -> loop(socket) end)
-    #	loop_accept(loop_socket)
-    #	
-    #  other ->
-    #	:ok
-    #end
   end
 
   defp loop(socket) do
@@ -52,16 +31,13 @@ defmodule Gophex.Worker do
 
   defp send_server_menu() do
     menu = Gophex.Agent.get(:main, :menu)
-    menu_to_string(menu)
+    |> menu_to_string()
   end
 
   defp menu_to_string(file_list) do
     case file_list do
-      [] ->
-	""
-	
       [ head ] ->
-	file_to_string(head) <> ".\r\n"
+	file_to_string(head) 
 
       [ head | tail ] ->
 	file_to_string(head) <> menu_to_string(tail)

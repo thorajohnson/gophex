ec523e99c9c03cbc7dd65d4b868a02e4ede8b8cf
kolljohnson <kjohnson@pillartechnology.com>
2017-08-11 13:31:42 -0500

    Add Receive Dir Test

--- test/gophex_test.exs
+++ test/gophex_test.exs
@@ -9,15 +9,26 @@ defmodule Gophex.GophexTest do
     #Gophex.Agent.start_link()
     #worker_pid = spawn_link(fn -> Gophex.Worker.accept(4040) end)
     assert {:ok, socket} = :gen_tcp.connect('0.0.0.0', 4040, [:binary, packet: 0, active: :false, reuseaddr: true])
+
     :gen_tcp.send(socket, "\r\n")
     {:ok, menu} = :gen_tcp.recv(socket, 0)
     assert is_binary(menu)
     {:ok, menu_files} = File.ls("files")
     assert String.first(menu) == "0" 
     assert String.contains?(menu, menu_files)
+
     :gen_tcp.send(socket, "gopher.txt\r\n")
     {:ok, file} = :gen_tcp.recv(socket, 0)
-    assert String.trim(file) == "Welcome to Gopher!" 
+    assert String.trim(file) == "Welcome to Gopher!"
+
+    :gen_tcp.send(socket, "\r\ntest_dir\r\n")
+    {:ok, dir} = :gen_tcp.recv(socket, 0)
+    {:ok, test_dir} = File.ls("test_dir")
+    assert String.first(dir) == "0"
+    assert String.contains?(dir, test_dir)
+    assert String.contains?(dir, "localhost")
+    
+    
     #:ok = :gen_tcp.close(socket)
     #Process.exit(worker_pid, :kill)
   end
